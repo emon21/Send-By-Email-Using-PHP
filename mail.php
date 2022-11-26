f5bd570332337c5dbc9de150fc967e2a93322721
emon21 <emonbd66@gmail.com>
2022-11-26 19:29:26 +0600

    feat:Email Send Into Localhost USing PHP CODE

---
+++ mail.php
@@ -0,0 +1,89 @@
+
+
+
+<?php
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+ 
+    //Load Composer's autoloader
+    // require 'vendor/autoload.php';
+ 
+    if (isset($_POST["send_mail"]))
+    {
+        // $name = $_POST["name"];
+        // $email = $_POST["email"];
+        // $password = $_POST["password"];
+
+        $email = $_POST['email'];
+     //   $body = $_POST['body'];
+ 
+        //Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+ 
+        try {
+            //Enable verbose debug output
+            $mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;
+ 
+            //Send using SMTP
+            $mail->isSMTP();
+ 
+            //Set the SMTP server to send through
+            $mail->Host = 'smtp.gmail.com';
+ 
+            //Enable SMTP authentication
+            $mail->SMTPAuth = true;
+ 
+            //SMTP username
+            $mail->Username = 'webdevhasib21@gmail.com';
+ 
+            //SMTP password
+            $mail->Password = 'swluufwqpluzuvst';
+ 
+            //Enable TLS encryption;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+ 
+            //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port = 587;
+ 
+            //Recipients
+            $mail->setFrom('webdevhasib21@gmail.com', 'SMARTBDSOFT');
+ 
+            //Add a recipient
+            $mail->addAddress($email);
+ 
+            //Set email format to HTML
+            $mail->isHTML(true);
+ 
+            $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+ 
+            $mail->Subject = 'Email verification';
+            $mail->Body    = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
+ 
+            $mail->send();
+            // echo 'Message has been sent';
+
+ 
+            // $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
+ 
+            // // connect with database
+            // $conn = mysqli_connect("localhost:8889", "root", "root", "test");
+ 
+            // // insert in users table
+            // $sql = "INSERT INTO users(name, email, password, verification_code, email_verified_at) VALUES ('" . $name . "', '" . $email . "', '" . $encrypted_password . "', '" . $verification_code . "', NULL)";
+            // mysqli_query($conn, $sql);
+ 
+            // header("Location: email-verification.php?email=" . $email);
+            // exit();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+?>
